a84211f5c0ce13a45437ef955e26c4f534c9123f
leeviko <leevikotilaine@gmail.com>
2023-04-21 10:49:10 +0300

    Make building a lot faster

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build
 SET deps=D:\Dev\Win\OpenGL
 SET includes=/I %deps%/includes /I src/includes /I src/vendor
 SET links=/link glfw3_mt.lib opengl32.lib freetype.lib kernel32.lib user32.lib gdi32.lib shell32.lib /LIBPATH:%deps%/libs 
-SET defines=/D DEBUG
+SET defines=/D DEBUG:FASTLINK
 
 echo "Building main..."
-cl /nologo /EHsc %includes% %defines% /Fobuild\ /Femain.exe src/*.c* %links%
+cl /MP /nologo /EHsc %includes% %defines% /Fobuild\ /Femain.exe src/*.c* %links%
